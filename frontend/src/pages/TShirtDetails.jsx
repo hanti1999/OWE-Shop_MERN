933e3c5ddf4cf005f0a8f8a8f0ad7f7200bb2913
Hoàng Anh <nth.anh020209@gmail.com>
2023-08-09 11:34:20 +0700

    add routers, files

---
+++ frontend/src/pages/TShirtDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TShirtDetails = () => {
+  return (
+    <div>TShirtDetails</div>
+  )
+}
+
+export default TShirtDetails
---
+++ frontend/src/router/Routers.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from '../App';
+import Home from '../pages/Home';
+import TShirt from '../pages/TShirt';
+import TShirtDetails from '../pages/TShirtDetails';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import SearchResultList from '../pages/SearchResultList';
+
+export const Routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/t-shirt',
+        element: <TShirt />,
+      },
+      {
+        path: '/t-shirt/:id',
+        element: <TShirtDetails />,
+      },
+      {
+        path: '/t-shirt/search',
+        element: <SearchResultList />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+    ],
+  },
+]);
