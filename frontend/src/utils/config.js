93926a9e4cb6f1d3915bb7909c787d30dd0a9cd4
Hoàng Anh <nth.anh020209@gmail.com>
2023-09-14 10:57:32 +0700

    add backend-api for deployment

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:4000/api/v1';
+export const BASE_URL = 'https://oweshop-backend.onrender.com/api/v1';
+// for local test
+// export const BASE_URL = 'http://localhost:4000/api/v1';
