993c91c24745124fc01f9cd7de504e7c868b6140
Hoàng Anh <nth.anh020209@gmail.com>
2024-07-12 19:10:18 +0700

    change api

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://oweshop-backend.onrender.com/api/v1';
+export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 // for local test
 // export const BASE_URL = 'http://localhost:4000/api/v1';
